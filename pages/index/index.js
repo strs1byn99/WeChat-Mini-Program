cb3a47aaf50d49d0b4be2bac57aabb303a1c9f6f
Perry Yang <byang33@ucsc.edu>
2020-04-05 18:17:29 -0700

    create js functions filtering

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,27 +2,28 @@
 
 const db = wx.cloud.database();
 const searchResult = db.collection("searchResult");
-searchResult.add({
-  data: {
-    "name": "Perry",
-    "age": 21,
-    "school": "ucsc"
-  }
-});
+// searchResult.add({
+//   data: {
+//     "title": "AAAA"
+//   }
+// }); 
 
 Page({
   /**
    * Page initial data
    */
   data: {
-
+    Source: ["weixin", "wechat", "android", "Android", "IOS", "java", "javascript", "微信小程序", "微信公众号", "微信开发者工具"],
+    bindSource: [],
+    history: []
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-
+    this.showHistory(); // Get 5 search histories
+    this.getArticles(); // Get all article names
   },
 
   /**
@@ -72,5 +73,52 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  getSearchResult: function () {
+    
+  },
+
+  getArticles: function () {
+    searchResult.get().then(res => {
+      var data = res.data;
+      // setData to Source
+    });
+  },
+
+  showHistory: function () { // OnLoad function
+    searchResult.count().then(res => {
+      // console.log(res.total);
+      searchResult.skip(res.total-5).limit(5).get().then(res => {
+        console.log(res.data);
+        // assign to pagedata
+      });
+    });
+  },
+
+  bindinput: function (e) {
+    var prefix = e.detail.value;  // real time input
+    var sources = []              // automated sources 
+    console.log(prefix);
+    if (prefix != "") {
+      // loop over all sources in Sources (later will update Sources with real data in DB)
+      this.data.Source.forEach(function (x) {
+        if (x.indexOf(prefix) != -1) {
+          console.log(x);
+          sources.push(x);
+        }
+      });
+    }
+    if (sources.length != 0) {
+      this.setData({
+        bindSource: sources
+      });
+      console.log(this.data.bindSource);
+    } 
+  },
+
+  bindconfirm(event){
+    console.log("hello");
+    console.log(event.detail.value);
   }
 })
